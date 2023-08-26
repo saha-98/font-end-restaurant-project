1441e008870bf23edc731b89eebefdfc5106680e
unknown <bishawjet.sahs@gmail.com>
2023-08-26 09:02:31 +0600

    About us Section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding "
+    id="aobut"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g latter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon" className="spoon__img" />
+        <p className="p__opensans">
+          We are acient to deploy our service to the next level. We are the best
+          and always will be.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Since 1990 at your service.Always good to serve you as always special
+          guest. You are our pride . And happy to serve you as our guest.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
+      </div>
+    </div>
   </div>
 );
 
