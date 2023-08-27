76ad19907afc1f78feeba0e6e66fa8fc780918cf
unknown <bishawjet.sahs@gmail.com>
2023-08-27 11:16:48 +0600

    The chef section with responive look

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef img" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's world" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote img" />
+          <p className="p__opensans">
+            The best thing dont need to advertizement. Some-People born with the
+            power of cooking. And we have that one with us
+          </p>
+        </div>
+
+        <p className="p__opensans">
+          Here We are . The most and nice cooking hand is looking for you to
+          sever the best foods
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign img" />
+      </div>
+    </div>
   </div>
 );
 
