e79e458fb922d6b6a5eabd80060043bc8d156fd6
unknown <bishawjet.sahs@gmail.com>
2023-08-29 16:14:14 +0600

    The footer section with responsiv look

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe to our Newsletter</h1>
+      <p className="p__opensans">And never miss the Upadates</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter Your Email adress" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
